b5eaccf999755b7cbae2d89c9cc2111956d1f0d2
WobbyChip <wobbychip.simik@gmail.com>
2021-07-26 17:53:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ io.sockets.on("connection", socket => {
 
             //If last player left delete room
             if (players_online > 0) {
-                if (!room.winner) { io.to(socket_room).emit("left", info); }
+                if (!room.winner) { io.sockets.to(socket_room).emit("left", info); }
             } else {
                 delete rooms[socket_room];
             }
